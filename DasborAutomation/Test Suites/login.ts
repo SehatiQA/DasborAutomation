9cb8a9db2319c7e300f54b64c02a11077b65d304
sehatiqa <sehatiqa@gmail.com>
2019-12-09 21:51:35 +0700

    add test suite, add configurable execution email of sehati dashboard

---
+++ DasborAutomation/Test Suites/login.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>f8b81873-9fa2-4739-8bdb-458d6af72df7</testSuiteGuid>
+   <testCaseLink>
+      <guid>d9327f90-b03b-4ecf-a9a0-54cf0dc021d4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login_02</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6dcb349e-d861-4835-815f-c2e3c7b5d8f3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login_03</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
