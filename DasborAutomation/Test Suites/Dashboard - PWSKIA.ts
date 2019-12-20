2f0bb2542613b3b962ef9400cd2762531f40da4b
sehatiqa <sehatiqa@gmail.com>
2019-12-20 16:39:59 +0700

    rudi : - add PWSKIA testsuite

---
+++ DasborAutomation/Test Suites/Dashboard - PWSKIA.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Dashboard - PWSKIA</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>9492d2a8-0d32-40d1-beea-0f46a666a9d5</testSuiteGuid>
+   <testCaseLink>
+      <guid>0a833f04-6d6a-42a7-b37c-60b0e13323f0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PWSKIA_DINKES_02</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
